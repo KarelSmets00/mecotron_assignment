f20e6df04a0ad02c351761db96109e031bf121b9
Karel Smets <r0761743@esat.kuleuven.be>
2021-11-10 21:59:24 +0100

    uitbreinding identificatie file naar positie model

--- Assignment 1/Model_Identification.m
+++ Assignment 1/Model_Identification.m
@@ -60,25 +60,26 @@ u_mean = mean(data(:,4,:),3)*voltage;
 
 figure(10)
 hold on
+box on
 plot(t,th_mean)
 plot(t,v_mean)
 plot(t,u_mean)
 xlabel('t [s]')
 legend('th','v','u')
+title('Measurements')
 
 
 %% identification of the simple model
 
 
 
-%% identification of the exacter model
-length = 80;
-shift = 10;
-Ts = 0.01;
-voltage = 3;
+%% identification of the more realistic model
+
+% -------------------------------------------------------------------------
+% ---- model for speed output ---- 
 
 % -- fft -- 
-th_f = fft(v_mean);
+v_f = fft(v_mean);
 u_f = fft(u_mean);
 
 fs = 1/Ts;
@@ -87,15 +88,19 @@ f = [0:(length-1)]*(fs/length);
 % frf opgesteld in fig3
 figure(20)
 subplot(2,1,1)
-box on
-semilogx(f, 20*log10(abs(th_f./u_f)))
+semilogx(f, 20*log10(abs(v_f./u_f)))
 hold on
+box on
+ylabel('mag. [dB]')
+xlabel('\omega [rad/s]')
 title('magnitude')
 
 subplot(2,1,2)
-box on
-semilogx(f, unwrap(angle(th_f./u_f))*180/pi)
+semilogx(f, unwrap(angle(v_f./u_f))*180/pi)
 hold on
+box on
+ylabel('ang. [°]')
+xlabel('\omega [rad/s]')
 title('phase')
 
 % -- least squ --
@@ -104,31 +109,101 @@ A = [-v_mean(5:(end-1)) -v_mean(4:(end-2)) -v_mean(3:(end-3)) -v_mean(2:(end-4))
 
 x = A\b;
 
-Num = [0 x(5:end)'];
-Den = [1 x(1:4)' 0];
+Num_1 = [0 x(5:end)'];
+Den_1 = [1 x(1:4)' 0];
 
-sys = tf(Num,Den,Ts)
+sys_1 = tf(Num_1,Den_1,Ts)
 
 figure(30)
 hold on
-steprp = lsim(sys,u_mean,t);
-plot(t,steprp)
+box on
+steprp_1 = lsim(sys_1,u_mean,t);
+plot(t,steprp_1)
 plot(t,v_mean)
 stairs(t,u_mean)
 xlabel('t [s]')
+legend('simulation','measurement','input')
+title('Speed Step Response')
 
 % vergelijk bode diagrammen
 
-H = squeeze(freqresp(sys,2*pi*f));
+H1 = squeeze(freqresp(sys_1,2*pi*f));
 
 figure(20)
 subplot(2,1,1)
 hold on
 box on
-semilogx(f, 20*log10(abs(H)))
+semilogx(f, 20*log10(abs(H1)))
 
 subplot(2,1,2)
 hold on
 box on
-semilogx(f, unwrap(angle(H))*180/pi)
+semilogx(f, unwrap(angle(H1))*180/pi)
+
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+% ---- model for position output ----
+
+% -- fft -- 
+th_f = fft(th_mean);
+u_f = fft(u_mean);
+
+fs = 1/Ts;
+f = [0:(length-1)]*(fs/length);
+
+% frf opgesteld in fig3
+figure(40)
+subplot(2,1,1)
+semilogx(f, 20*log10(abs(th_f./u_f)))
+hold on
+box on
+ylabel('mag. [dB]')
+xlabel('\omega [rad/s]')
+title('magnitude')
+
+subplot(2,1,2)
+semilogx(f, unwrap(angle(th_f./u_f))*180/pi)
+hold on
+box on
+ylabel('ang. [°]')
+xlabel('\omega [rad/s]')
+title('phase')
+
+% -- least squ --
+b = th_mean(7:end);
+A = [-th_mean(6:(end-1)) -th_mean(5:(end-2)) -th_mean(4:(end-3)) -th_mean(3:(end-4)) -th_mean(2:(end-5)) u_mean(6:(end-1)) u_mean(5:(end-2)) u_mean(4:(end-3)) u_mean(3:(end-4)) u_mean(2:(end-5)) u_mean(1:(end-6))];
+
+x = A\b;
+
+Num_2 = [0 x(6:end)'];
+Den_2 = [1 x(1:5)' 0];
+
+sys_2 = tf(Num_2,Den_2,Ts)
+
+figure(50)
+hold on
+box on
+steprp_2 = lsim(sys_2,u_mean,t);
+plot(t,steprp_2)
+plot(t,th_mean)
+stairs(t,u_mean)
+xlabel('t [s]')
+legend('simulation','measurement','input')
+title('Position Step Response')
+
+% vergelijk bode diagrammen
+
+H2 = squeeze(freqresp(sys_2,2*pi*f));
+
+figure(40)
+subplot(2,1,1)
+hold on
+box on
+semilogx(f, 20*log10(abs(H2)))
+
+subplot(2,1,2)
+hold on
+box on
+semilogx(f, unwrap(angle(H2))*180/pi)
+
 

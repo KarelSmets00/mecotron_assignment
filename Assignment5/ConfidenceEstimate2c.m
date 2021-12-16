1bd3027accde7a45b22c76dfbc9db5524628dcb8
Karel Smets <r0761743@esat.kuleuven.be>
2021-12-16 10:10:55 +0100

    moved assignement 4 to 5

--- Assignment5/ConfidenceEstimate2c.m
+++ Assignment5/ConfidenceEstimate2c.m
@@ -10,8 +10,12 @@ states = [1 2 3];
 confidence = 0.95;
 
 % -- FILES --
-files = [".\measured data\Vraag2C\...",         9.1599e-7;
-         ".\measured data\Vraag2C\...",         9.1599e-7];
+files = [".\measured data\QR_optimisation\Q1e-6.csv"
+         ".\measured data\QR_optimisation\Q5e-7.csv"
+         ".\measured data\QR_optimisation\Q5e-9.csv"];
+
+R = [1e-6 0
+     0    1e-6];
 
 % -- processing --
 
@@ -20,8 +24,7 @@ for i = 1:length(files(:,1))
     % clean up
     clear readdata readlabels obj
     
-    filename = files(i,1);
-    R = files(i,2);
+    filename = files(i);
     csvfile = convertStringsToChars(filename);
     
     % label order: P11, P12, P13, P22, P23, P33, x1, x2, x3, y1, y2
@@ -34,28 +37,29 @@ for i = 1:length(files(:,1))
     readlabels = strsplit(readlabels{:,datastartline-1},[delimiter ' ']);
     readdata = dlmread(csvfile, delimiter, datastartline-1, 0);  % data
 
-    i_start = find(readdata(:,10)==-0.15,1)+1;
-    readdata = readdata((i_start:end),:);
+%     i_start = find(readdata(:,10)==-0.15,1)+1;
+%     readdata = readdata((i_start:end),:);
     
     [lia,locb] = ismember(labels,readlabels);
     readdata = readdata(:,[1 locb]);
     
-    R = repmat(R,[1 1 size(readdata,1)]);
+    r = repmat(R,[1 1 size(readdata,1)]);
     u = zeros(0,size(readdata,1));
+    P = reshape([readdata(:,2:4) readdata(:,3) readdata(:,5:6) readdata(:,4) readdata(:,6) readdata(:,7)]', [3,3,size(readdata,1)]);
     
-    obj = KalmanExperiment(readdata(:,1)/1000, readdata(:,3)', permute(readdata(:,2),[2 3 1]), readdata(:,4)', R, u, readdata(:,5)', permute(readdata(:,6),[2 3 1]));     
-    [prob_nis, prob_snis] = analyzeconsistency(obj, confidence, M);
+    obj = KalmanExperiment(readdata(:,1)/1000, readdata(:,8:10)', P, readdata(:,11:12)', r, u);      
+    plotstates(obj, states, confidence);
     
-    filename = strsplit(filename,'\');
-    measurementName = erase(filename(end),'.csv');
-    
-    if export_fig
-        fig = gcf;
-        fig.PaperUnits = 'inches';
-        fig.PaperPosition = [0 0 6 3];
-        print(measurementName,'-dpng','-r0')
-        movefile(".\"+measurementName+".png", ".\NIS_SNISS")
-    end 
+%     filename = strsplit(filename,'\');
+%     measurementName = erase(filename(end),'.csv');
+%     
+%     if export_fig
+%         fig = gcf;
+%         fig.PaperUnits = 'inches';
+%         fig.PaperPosition = [0 0 6 3];
+%         print(measurementName,'-dpng','-r0')
+%         movefile(".\"+measurementName+".png", ".\StatePlots")
+%     end 
     
 end
 
